eb873ec7a36fd3730ef322dd7ffbc15cc8543efc
Samuel981 <ssoares981@gmail.com>
2024-09-07 19:29:06 -0300

    corrige aspecto dos icones

--- Assets/MyAssets/Scripts/Interface/ImgButtonController.cs
+++ Assets/MyAssets/Scripts/Interface/ImgButtonController.cs
@@ -1,17 +1,11 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
-using System.IO;
 using UnityEngine.InputSystem;
-using TMPro;
 
 
-public class ImgButtonController : UIController
-{
+public class ImgButtonController : UIController{
     private Vector2 lastMousePosition;
     public bool gamepadOn = false;
-    private Gamepad gamepad;
 
     private enum Input {PC, GAMEPAD};
 
@@ -24,31 +18,21 @@ public class ImgButtonController : UIController
     [Header("Imgs Gamepad")]
     public Sprite[] sprites_gamepad;
 
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
+    void Update(){
         checkInput();
-        if (gamepadOn){
-            //print("Controle");
+        if (gamepadOn)
             ToggleIcons(Input.GAMEPAD);
-        }
         else
             ToggleIcons(Input.PC);
     }
 
-    private void checkInput()
-    {
+    private void checkInput(){
         bool mouseMove = false;
         Vector2 currentMousePosition = new Vector2(Mouse.current.position.ReadValue().x, Mouse.current.position.ReadValue().y);
         if (currentMousePosition != lastMousePosition){
             if (lastMousePosition != Vector2.zero)
                 mouseMove = true;
+
             lastMousePosition = Mouse.current.position.ReadValue();
         }
 
@@ -74,9 +58,19 @@ public class ImgButtonController : UIController
 
         int index = 0;
         foreach (Image img in imgs){
-            if (img != null && img.gameObject.activeSelf)
+            if (img != null && img.gameObject.activeSelf) {
                 img.sprite = icons[index];
+                img.rectTransform.sizeDelta = setAspectoRatio(icons[index], img);
+            }
             index++;
         }
+    }
+
+    private Vector2 setAspectoRatio(Sprite sprite, Image img){
+        float originalWidth = sprite.texture.width;
+        float originalHeight = sprite.texture.height;
+        float height = img.rectTransform.rect.height;
+        float width = (originalWidth / originalHeight) * height;
+        return new Vector2(width, height);
     }
 }
